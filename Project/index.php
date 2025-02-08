78b2941a0ea7651f166e4b0b77afcab7290dfe1b
nmktth <masi.masi.2006@mail.ru>
2025-02-08 11:44:36 +0300

    catscheck

--- Project/index.php
+++ Project/index.php
@@ -1,8 +1,17 @@
 <?php
     class Cat{
-        public $name
-        public $weight
-        public $color   
+        public $name;
+        public $weight;
+        public $color;
+
+        function setName(string $name){
+            $this->name = $name;
+        }
     }
 
-?>
+    $cat1 = new Cat();
+    $cat1->name = "Maikl";
+    $cat1->weight = 100;
+    $cat1->color = "gray";
+
+    var_dump($cat1);
