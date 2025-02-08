ecbc276ba5e557dc7e66a53c8677d49a1e53b013
nmktth <masi.masi.2006@mail.ru>
2025-02-08 11:57:04 +0300

    nice-cats-more-good

--- Project/index.php
+++ Project/index.php
@@ -4,8 +4,10 @@
         public $weight;
         public $color;
 
-        function __construct(){
-            
+        function __construct(string $name,int $weight,string $color){
+            $this ->name = $name;
+            $this ->weight = $weight;
+            $this ->color = $color;
         }
 
         function setName(string $name){
@@ -17,14 +19,8 @@
         }
     }
 
-    $cat1 = new Cat();
-    $cat2 = new Cat();
-    $cat2->name = "Morivan";
-    $cat1->name = "Maikl";
-    $cat2->weight = 4;
-    $cat1->weight = 100;
-    $cat2->color = "blue";
-    $cat1->color = "gray";
+    $cat1 = new Cat('Maikl', 100, 'gray');
+    $cat2 = new Cat('Morivan', 10, 'blue');
 
     echo $cat2->getName(),'<BR>';
     echo $cat1->getName(),'<BR>';
